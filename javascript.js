b313449ae84bf209847bbf84bd22f11272144137
Cecilia E.T. Söderblom <ceciliasoderblom@Cecilias-MacBook-Pro-2.local>
2019-04-02 10:49:50 +0200

    post data added

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/* document.addEventListener("DOMContentLoaded", alertFunction());
+document.addEventListener("DOMContentLoaded", alertFunction());
 function alertFunction() {
   alert("OBS: This is a school project and not an official Volt website");
-} */
+}
